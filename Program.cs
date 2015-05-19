fc0a842e341a98553b1707ce856ca7c4711b3cdd
antonioi123 <antonioi@eslabon.com.mx>
2015-05-19 12:07:19 -0500

    Se agrego otra linea

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hola mundo");
+            System.Console.WriteLine("Yuka");
         }
     }
 }
